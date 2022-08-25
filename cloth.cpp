8948fae2eb81e2b2ee27bdfdf200c3f32d43b3ca
Shay P.C <3180102686@zju.edu.cn>
2022-08-25 13:41:02 -0600

    import eigen and construct sparse matrix K

--- cloth.cpp
+++ cloth.cpp
@@ -15,10 +15,9 @@ static const float dh=0.02f,epsilon=1e-6f;
 
 void Rod::vertex(){
   vtxs.resize((N_partition + 1) * (N_diagon + 1));
-  // FIXME: resize?
   float z = length / N_partition;
   for(int i=0;i<N_partition+1;i++){
-    float M_inv = i? 1.0f:0.0f;
+    float M_inv = i < N_partition? 1.0f:0.0f;
     vtxs[i * (N_diagon +1)] = Vertex(glm::vec3(0.f, i * z, 0.f), M_inv);
     for(int j =0;j<N_diagon;j++){
       int I = i * (N_diagon+1) + j + 1;
@@ -32,8 +31,6 @@ void Rod::vertex(){
   #endif
 }
 void Rod::connectivity(){
-  // ttns.resize(3 * N_diagon * N_partition);
-  // FIXME: no default constructor
   for(int i=0;i<N_partition;i++){
     for(int j = 0;j<N_diagon;j++){
       int K = i * (N_diagon +1);
@@ -79,6 +76,18 @@ void Rod::step(float dt){
   }
 }
 
+void Rod::build_sparse(){
+  for(auto &e: ttns){
+    for(int j=0; j < 4; j++){
+      e.compute_force_differentials(j,A, vtxs);
+      // FIXME: no use setting the matrix at the pinned points 
+    }
+  }
+  for(int i=0;i<n;i++){
+    // K_ii += 1 for mass matrix
+  }
+}
+
 void Tetrahedron::precomputation(){
   glm::mat3 Dm(i.pos-l.pos, j.pos-l.pos, k.pos-l.pos);
   Bm = glm::inverse(Dm); 
@@ -95,36 +104,56 @@ void Tetrahedron::compute_elastic_forces(){
   l.f += -(H[0] + H[1] + H[2]);
 }
 
-void Tetrahedron::compute_force_differentials(int _i){
-  // batch computation for f_ij for all vertices 
+inline void put(SparseMatrix<float> &K, int i, int col, glm::vec3 &df){
+  for(int k=0;k<3;k++){
+    int row = 3* i + k;
+    K.insert(row, col) = df[k];
+  }
+}
+void Tetrahedron::compute_force_differentials(int _j,  SparseMatrix<float> &K, std::vector<Vertex> &v){
+  // batch computation parital f_i for all v_i adjacent to v_j (in this tet) 
   glm::mat3 Ds(i.pos-l.pos, j.pos-l.pos, k.pos-l.pos);
   glm::mat3 F = Ds * Bm;
-  for(int j = 0; j < 3; j++){
+  for(int k = 0; k < 3; k++){
+    // like ti.static
     glm::vec3 d_v[4];
-    d_v[_i][j] = 1.0f;
+    d_v[_j][k] = 1.0f;
     glm::mat3 d_Ds(d_v[0] - d_v[3], d_v[1] - d_v[3], d_v[2] - d_v[3]);
     glm::mat3 dF = d_Ds * Bm;
     glm::mat3 dP = differential_piola(F, dF);
     glm::mat3 dH = - W * dP *glm::transpose(Bm);
-    df[0][_i][j] += dH[0];
-    df[1][_i][j] += dH[1];
-    df[2][_i][j] += dH[2];
-    df[3][_i][j] += -(dH[0] + dH[1] + dH[2]);
+    df[0] = dH[0];
+    df[1] = dH[1];
+    df[2] = dH[2];
+    df[3] = -(dH[0] + dH[1] + dH[2]);
+
+    /* put vec3 to 
+    row = [3i, 3i+2]
+    col = 3j + k
+    */
+   int J = index[_j];
+   if(v[J].M_inv == 0) return;
+   int col = 3 * J + k;
+   for(int _i=0;_i<4; _i ++){
+    int i = index[_i];
+    if(v[i].M_inv == 0) continue;
+    put(K, i , col, df[_i]);
+   }
   }
 }
 glm::mat3 Tetrahedron::piola_tensor(glm::mat3 &F){
   // linear elasticity
   // return (F + glm::transpose(F) - glm::mat3(1.0f) * 2.0f) * mu + glm::mat3(1.0f) * lambda * (F[0][0] + F[1][1] + F[2][2] - 3.0f);
   // neo-hookean
-  auto F_inv_T = glm::transpose(glm::inverse(F));
-  return mu * (F - F_inv_T) + lambda * log(glm::determinant(F)) / 2.0f * F_inv_T;
+  auto Fjnv_T = glm::transpose(glm::inverse(F));
+  return mu * (F - Fjnv_T) + lambda * log(glm::determinant(F)) / 2.0f * Fjnv_T;
 }
 
 glm::mat3 Tetrahedron::differential_piola(glm::mat3 &F, glm::mat3 &dF){
-  auto F_inv_T = glm::transpose(glm::inverse(F));
+  auto Fjnv_T = glm::transpose(glm::inverse(F));
   glm::mat3 B = glm::inverse(F) * dF;
   float tr = B[0][0] + B[1][1] + B[2][2];
-  return mu * dF + (mu - lambda * log(glm::determinant(F))) * F_inv_T * glm::transpose(dF) * F_inv_T + lambda * tr * F_inv_T;
+  return mu * dF + (mu - lambda * log(glm::determinant(F))) * Fjnv_T * glm::transpose(dF) * Fjnv_T + lambda * tr * Fjnv_T;
 }
 
 void Cloth::reset(){
@@ -260,7 +289,6 @@ void Constrain::solve(){
     case 1:
       // l=length(m[0]->tmp-ext_obj->tmp);
       // C0=m[0]->w/(m[0]->w+ext_obj->w);C1=1.0f-C0;
-      // // FIXME: null , 
       // if(l<r+dh){
       //   m[0]->tmp+=m[0]->tmp*(r+dh-l)/l*C0;
       //   ext_obj->tmp-=m[0]->tmp*(r+dh-l)/l*C1;
--- global.h
+++ global.h
@@ -1,6 +1,9 @@
 #include <glm/glm.hpp>
+#include "Eigen/Eigen"
+#include "Eigen/Sparse"
 #include <GL/glut.h>
 #include <vector>
+using namespace Eigen;
 struct Ball{
   float radius;
   Ball(float radius=0.6f):radius(radius){}
@@ -63,16 +66,27 @@ struct Tetrahedron{
   float W;
   Tetrahedron(std::vector<Vertex> &v,int i,int j,int k,int l):i(v[i]),j(v[j]),k(v[k]),l(v[l]){
     precomputation();
+    rem_index(i, j, k, l);
   }
   Tetrahedron(const Tetrahedron &a):i(a.i),j(a.j),k(a.k),l(a.l){
     precomputation();
+    for(int i=0; i < 4; i++){
+      index[i] = a.index[i];
+    }
+  }
+  inline void rem_index(int i,int j,int k,int l){
+    index[0] = i;
+    index[1] = j;
+    index[2] = k;
+    index[3] = l;
   }
   void precomputation();
   void compute_elastic_forces();
-  void compute_force_differentials(int i);
+  void compute_force_differentials(int j, SparseMatrix<float> &K,std::vector<Vertex> &v);
   glm::mat3 piola_tensor(glm::mat3 &F);
   glm::mat3 differential_piola(glm::mat3 &F, glm::mat3 &dF);
-  glm::vec3 df[4][4][3];
+  glm::vec3 df[4];
+  int index[4];
   inline void draw(){
     static glm::vec3 color[4]={
       glm::vec3(1.0f,1.0f,1.0f),
@@ -109,9 +123,6 @@ struct Tetrahedron{
   glEnd();
   }
 };
-// glm::mat3 operator *(glm::mat3 A, double x){
-//   return A;
-// }
 struct Rod{
   float radius, length;
   int N_diagon, N_partition;
@@ -120,10 +131,25 @@ struct Rod{
   void vertex();
   void connectivity();
   // Rod(float radius = 0.6f, float length = 2.0f,int N_diagon = 8,int N_partition = 4):radius(radius),N_diagon(N_diagon),N_partition(N_partition),length(length){
-  Rod(float radius = 0.1f, float length = 1.0f,int N_diagon = 8,int N_partition = 20):radius(radius),N_diagon(N_diagon),N_partition(N_partition),length(length){
+  Rod(float radius = 0.1f, float length = 1.0f,int N_diagon = 8,int N_partition = 20):radius(radius),N_diagon(N_diagon),N_partition(N_partition),length(length),n(N_partition*(N_diagon + 1)),x(n),b(n){
     vertex();
     connectivity();
     // reset();
+  }
+  int n;
+  VectorXf x, b;
+  SparseMatrix<float> A;
+  SparseLU<SparseMatrix<float>, COLAMDOrdering<int> >   solver;
+
+  void build_sparse();
+  // fill A and b;
+  void solve(){
+    // Compute the ordering permutation vector from the structural pattern of A
+    solver.analyzePattern(A); 
+    // Compute the numerical factorization 
+    solver.factorize(A); 
+    //Use the factors to solve the linear system 
+    x = solver.solve(b);  
   }
   static const glm::vec3 color;
   void draw();
